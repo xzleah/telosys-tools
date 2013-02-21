38a11eafd14c21c4258f464e574b93e802c1f1d7
laurent.guer1 <laurent.guer1@gmail.com>
2013-02-21 09:58:00 +0000

    Update StrUtil

---
+++ telosys-tools-commons/src/test/java/org/telosys/tools/tests/commons/StrUtilTests.java
@@ -0,0 +1,23 @@
+package org.telosys.tools.tests.commons;
+
+import org.telosys.tools.commons.StrUtil;
+
+import junit.framework.TestCase;
+
+public class StrUtilTests extends TestCase {
+
+	public void testDifferent() {
+		assertTrue( StrUtil.different("aa", "bb") ) ;
+		assertTrue( StrUtil.different(null, "bb") ) ;
+		assertTrue( StrUtil.different(null, "") ) ;
+		assertTrue( StrUtil.different("aa", null) ) ;
+		assertTrue( StrUtil.different("", null) ) ;
+
+		assertFalse( StrUtil.different("aa", "aa") ) ;
+		assertFalse( StrUtil.different("",   "") ) ;
+		assertFalse( StrUtil.different(null, null) ) ;
+
+	}
+
+
+}
