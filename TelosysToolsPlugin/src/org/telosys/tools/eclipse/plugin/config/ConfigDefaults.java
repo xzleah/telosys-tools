8c09684e1bed56bec0d3f7829ae17b38476ffc81
laurent.guer1 <laurent.guer1@gmail.com>
2013-03-01 17:35:57 +0000

    Ver 2.0.4
    config page :
    . init $DOC, $TMP
    . init package : org.demo.bean
    . + Download folder
    . + [Init Telosys Tools]  ( folders creation + databases.dbcfg copy )

--- TelosysToolsPlugin/src/org/telosys/tools/eclipse/plugin/config/ConfigDefaults.java
+++ TelosysToolsPlugin/src/org/telosys/tools/eclipse/plugin/config/ConfigDefaults.java
@@ -8,18 +8,18 @@ package org.telosys.tools.eclipse.plugin.config;
  */
 public class ConfigDefaults {
 
-	public final static String BEANNAME = "BEANNAME" ;
+//	public final static String BEANNAME = "BEANNAME" ;
 	//---------------------------------------------------------------------------
 	// Default classe names
 	//---------------------------------------------------------------------------
  
-	 public final static String DEFAULT_BEAN_CLASS_NAME       =  "${" + BEANNAME + "}" ;
+//	 public final static String DEFAULT_BEAN_CLASS_NAME       =  "${" + BEANNAME + "}" ;
 	 
-	 public final static String DEFAULT_LIST_CLASS_NAME       =  "${" + BEANNAME + "}List" ;
+//	 public final static String DEFAULT_LIST_CLASS_NAME       =  "${" + BEANNAME + "}List" ;
 
-	 public final static String DEFAULT_DAO_CLASS_NAME        =  "${" + BEANNAME + "}DAO" ;
+//	 public final static String DEFAULT_DAO_CLASS_NAME        =  "${" + BEANNAME + "}DAO" ;
 	 
-	 public final static String DEFAULT_XML_MAPPER_CLASS_NAME =  "${" + BEANNAME + "}Xml" ;
+//	 public final static String DEFAULT_XML_MAPPER_CLASS_NAME =  "${" + BEANNAME + "}Xml" ;
 	 
 	 //---------------------------------------------------------------------------
 	 
