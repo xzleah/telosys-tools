76f00fc81fb89ac60d0f110690595ed4ce73c7a9
laurent.guer1 <laurent.guer1@gmail.com>
2013-07-15 14:21:57 +0000

    Version 2.0.5

---
+++ ver2.0.5/src/main/java/org/telosys/tools/generator/config/GeneratorConfig.java
@@ -0,0 +1,176 @@
+/**
+ *  Copyright (C) 2008-2013  Telosys project org. ( http://www.telosys.org/ )
+ *
+ *  Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.gnu.org/licenses/lgpl.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.telosys.tools.generator.config;
+
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Properties;
+
+import org.telosys.tools.commons.Variable;
+import org.telosys.tools.commons.VariablesUtil;
+import org.telosys.tools.generator.ContextName;
+import org.telosys.tools.generator.context.ProjectConfiguration;
+
+/**
+ * Generator configuration implementation 
+ * 
+ * @author Laurent GUERIN
+ *
+ */
+public class GeneratorConfig implements IGeneratorConfig
+{
+	public static final String PROJECT_CONFIG_FILE = "telosys-tools.cfg" ;
+	
+	private final String     _sProjectLocation ; // Project folder ( absolute path )
+	
+	private final String     _sTemplatesFolder ;
+	
+	private final String     _sEntityClassPackage ;
+
+	private final Variable[] _projectVariables ;
+	
+	public GeneratorConfig(String sProjectLocation, Properties prop) 
+	{
+		_sProjectLocation = sProjectLocation;
+		
+		//--- Files folders
+    	_sTemplatesFolder     = prop.getProperty(GeneratorConfigConst.TEMPLATES_FOLDER, null);
+		
+		//--- Packages names
+    	_sEntityClassPackage  = prop.getProperty(GeneratorConfigConst.ENTITIES_PACKAGE, null);
+    	
+    	//--- All variables : specific project variables + folders 
+    	Hashtable<String, String> allVariables = new Hashtable<String, String>();
+    	
+    	//--- 1) Project user defined variables
+    	Variable[] specificVariables = VariablesUtil.getVariablesFromProperties( prop );
+    	for ( Variable v : specificVariables ) {
+    		allVariables.put(v.getName(), v.getValue());
+    	}
+    	//--- 2) Folders ( at the end to override specific variables if any )
+    	allVariables.put( ContextName.SRC,      prop.getProperty(ContextName.SRC,      "") );
+    	allVariables.put( ContextName.RES,      prop.getProperty(ContextName.RES,      "") );
+    	allVariables.put( ContextName.WEB,      prop.getProperty(ContextName.WEB,      "") );
+    	allVariables.put( ContextName.TEST_SRC, prop.getProperty(ContextName.TEST_SRC, "") );
+    	allVariables.put( ContextName.TEST_RES, prop.getProperty(ContextName.TEST_RES, "") );
+    	allVariables.put( ContextName.DOC,      prop.getProperty(ContextName.DOC,      "") );
+    	allVariables.put( ContextName.TMP,      prop.getProperty(ContextName.TMP,      "") );
+    	
+    	//--- 3) Get all variables to build the array
+    	LinkedList<Variable> variablesList = new LinkedList<Variable>();
+    	for ( String varName : allVariables.keySet() ) {
+    		String varValue = allVariables.get(varName) ;
+    		variablesList.add( new Variable( varName, varValue) ) ;
+    	}
+    	Variable[] allVariablesArray = variablesList.toArray( new Variable[0] );
+    	
+    	_projectVariables = allVariablesArray ;
+    	
+	}
+
+//	//---------------------------------------------------------------------
+//	// Specific variables
+//	//---------------------------------------------------------------------
+//	public Variable[] getProjectVariables() 
+//	{
+//		return _projectVariables ;
+//	}
+
+	//---------------------------------------------------------------------
+	// Project Configuration for Generator context
+	//---------------------------------------------------------------------
+	public ProjectConfiguration getProjectConfiguration() 
+	{
+		ProjectConfiguration projectConfiguration = new ProjectConfiguration( 
+				//_sSourceFolder, _sWebContentFolder, 
+				getTemplatesFolderFullPath(),
+				_sEntityClassPackage, 
+				_projectVariables );
+		
+		return projectConfiguration;
+	}
+
+	//---------------------------------------------------------------------
+	// Folders
+	//---------------------------------------------------------------------
+    public String getProjectLocation()
+	{
+    	return _sProjectLocation ;
+	}
+    
+    /**
+     * Returns the "Project Full Path" by adding the given subpath at the end of the project location
+     * @param sSubPath the "sub path" to add
+     * @return
+     */
+    private String getProjectFullPath(String sSubPath)
+	{
+    	if ( sSubPath != null )
+    	{
+    		String s = sSubPath.trim() ;
+    		if ( s.startsWith("/") || s.startsWith("\\") )
+    		{
+    	    	return _sProjectLocation + s ;
+    		}
+    		else
+    		{
+    	    	return _sProjectLocation + "/" + s ;
+    		}
+    	}
+    	else
+    	{
+    		return _sProjectLocation ;
+    	}
+	}
+    
+	
+    /**
+     * Returns the "full path" templates folder (never null)<br>
+     * e.g. : "C:/dir/workspace/project/folder"
+     * It can be the plugin templates folder ( if the template folder property is not set )
+     * or the project templates folder
+     * @return
+     */
+    public String getTemplatesFolderFullPath()
+	{
+    	if ( _sTemplatesFolder != null )
+    	{
+    		String s = _sTemplatesFolder.trim() ;
+    		if ( s.length() == 0 )
+    		{
+    			// No templates folder 
+    			return null ;
+    		}
+    		else
+    		{
+    			// Templates folder is set => build the full path
+    			return getProjectFullPath(_sTemplatesFolder) ;
+    		}
+    	}
+    	else
+    	{
+    		return null ;
+    	}
+	}
+
+//	//---------------------------------------------------------------------
+//	// Packages
+//	//---------------------------------------------------------------------
+//	public String getVOPackage() {
+//		return _sPackageVO ;
+//	}
+
+}
---
+++ ver2.0.5/src/main/java/org/telosys/tools/generator/context/VariableNames.java
@@ -0,0 +1,219 @@
+/**
+ *  Copyright (C) 2008-2013  Telosys project org. ( http://www.telosys.org/ )
+ *
+ *  Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.gnu.org/licenses/lgpl.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.telosys.tools.generator.context;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.telosys.tools.commons.Variable;
+import org.telosys.tools.generator.ContextName;
+
+/**
+ * Reserved variable names
+ *  
+ * @author L. Guerin
+ *
+ */
+public class VariableNames {
+	
+	private final static String[] VOID_STRING_ARRAY = {} ;
+	
+	private static List<String> VARIABLES_LIST = new LinkedList<String>();
+	static {
+		//--- Special characters 
+		VARIABLES_LIST.add( ContextName.DOLLAR );
+		VARIABLES_LIST.add( ContextName.SHARP  );
+		VARIABLES_LIST.add( ContextName.AMP    );
+		VARIABLES_LIST.add( ContextName.QUOT   );
+		VARIABLES_LIST.add( ContextName.LT     );
+		VARIABLES_LIST.add( ContextName.GT     );
+		VARIABLES_LIST.add( ContextName.LBRACE );
+		VARIABLES_LIST.add( ContextName.RBRACE );
+		
+		//--- FOLDERS predefined variables names ( v 2.0.3 )
+		VARIABLES_LIST.add( ContextName.SRC );
+		VARIABLES_LIST.add( ContextName.RES );
+		VARIABLES_LIST.add( ContextName.WEB );
+		VARIABLES_LIST.add( ContextName.TEST_SRC );
+		VARIABLES_LIST.add( ContextName.TEST_RES );
+		VARIABLES_LIST.add( ContextName.DOC );
+		VARIABLES_LIST.add( ContextName.TMP );
+		
+		Collections.sort(VARIABLES_LIST);
+	}
+
+	private static List<String> GENERATOR_OBJECTS_LIST = new LinkedList<String>();
+	static {
+		//--- Invariable objects 
+		GENERATOR_OBJECTS_LIST.add( ContextName.CONST ); 
+		GENERATOR_OBJECTS_LIST.add( ContextName.FN );
+		GENERATOR_OBJECTS_LIST.add( ContextName.GENERATOR ); 
+		GENERATOR_OBJECTS_LIST.add( ContextName.LOADER );
+		GENERATOR_OBJECTS_LIST.add( ContextName.PROJECT );
+		GENERATOR_OBJECTS_LIST.add( ContextName.TODAY );
+
+		//--- Current Entity/Target objects
+		GENERATOR_OBJECTS_LIST.add( ContextName.TARGET      );
+		GENERATOR_OBJECTS_LIST.add( ContextName.BEAN_CLASS  ); // old name
+		GENERATOR_OBJECTS_LIST.add( ContextName.ENTITY      ); // new name
+		GENERATOR_OBJECTS_LIST.add( ContextName.SELECTED_ENTITIES  );
+
+		Collections.sort(GENERATOR_OBJECTS_LIST);
+	}
+
+	private static List<String> PREDEFINED_NAMES_LIST = new LinkedList<String>();
+	static {
+		PREDEFINED_NAMES_LIST.add( "attribute" ); 
+		PREDEFINED_NAMES_LIST.add( "attrib" ); 
+		PREDEFINED_NAMES_LIST.add( "field" ); 
+		
+		PREDEFINED_NAMES_LIST.add( "link" ); 
+	}
+
+	private static List<String> WIZARDS_OBJECTS_LIST = new LinkedList<String>();
+	static {
+		//--- Invariable objects 
+		WIZARDS_OBJECTS_LIST.add( ContextName.CLASS  );
+//		"context", 
+//		"screendata",
+//		"triggers"
+
+	}
+	
+	private static List<String> RESERVED_NAMES_LIST = new LinkedList<String>();
+	static {
+		for ( String s : VARIABLES_LIST ) {
+			RESERVED_NAMES_LIST.add(s);
+		}
+		for ( String s : GENERATOR_OBJECTS_LIST ) {
+			RESERVED_NAMES_LIST.add(s);
+		}
+		for ( String s : PREDEFINED_NAMES_LIST ) {
+			RESERVED_NAMES_LIST.add(s);
+		}
+		for ( String s : WIZARDS_OBJECTS_LIST ) {
+			RESERVED_NAMES_LIST.add(s);
+		}
+		Collections.sort(RESERVED_NAMES_LIST);
+	}
+	
+	private static List<String> VARIABLE_AND_OBJECT_NAMES_LIST = new LinkedList<String>();
+	static {
+		for ( String s : VARIABLES_LIST ) {
+			VARIABLE_AND_OBJECT_NAMES_LIST.add(s);
+		}
+		for ( String s : GENERATOR_OBJECTS_LIST ) {
+			VARIABLE_AND_OBJECT_NAMES_LIST.add(s);
+		}
+		Collections.sort(VARIABLE_AND_OBJECT_NAMES_LIST);
+	}
+	
+	//private final static String[] RESERVED_NAMES_ARRAY = RESERVED_NAMES_LIST.toArray( VOID_STRING_ARRAY );
+
+	public final static String[] getVariableNames()
+	{
+		return VARIABLES_LIST.toArray( VOID_STRING_ARRAY );
+	}
+	
+	public final static String[] getObjectNames()
+	{
+		return GENERATOR_OBJECTS_LIST.toArray( VOID_STRING_ARRAY );
+	}
+	
+	public final static String[] getObjectAndVariableNames()
+	{
+		return VARIABLE_AND_OBJECT_NAMES_LIST.toArray( VOID_STRING_ARRAY );
+	}
+	
+	public final static String[] getPredefinedNames()
+	{
+		return PREDEFINED_NAMES_LIST.toArray( VOID_STRING_ARRAY );
+	}
+	
+	/**
+	 * Returns a copy of all the variable names used in the Velocity Context
+	 * @return
+	 */
+	public final static String[] getReservedNames()
+	{
+//		int n = RESERVED_NAMES_ARRAY.length;
+//		String[] newArray = new String[ n ] ;
+//		System.arraycopy(RESERVED_NAMES_ARRAY, 0, newArray, 0, n);
+//		return newArray ;
+		return RESERVED_NAMES_LIST.toArray( VOID_STRING_ARRAY );
+	}
+	
+	/**
+	 * Returns a sorted copy of all the variable names used in the Velocity Context
+	 * @return
+	 */
+	public final static String[] getSortedReservedNames()
+	{
+		String[] names = getReservedNames() ;
+		Arrays.sort(names);
+		return names ;
+	}
+	
+	/**
+	 * Returns true if the given string is a variable name used in the Velocity Context
+	 * @param s
+	 * @return
+	 */
+	public final static boolean isReservedName(String s)
+	{
+		if ( s != null ) {
+			for ( String reserved : RESERVED_NAMES_LIST ) {
+				if ( s.equals( reserved ) ) {
+					return true ;
+				}
+			}
+//			for ( int i = 0 ; i < RESERVED_NAMES_ARRAY.length ; i++ )
+//			{
+//				if ( s.equals( RESERVED_NAMES_ARRAY[i] ) ) return true ;
+//			}
+		}
+		return false ;
+	}
+	
+	/**
+	 * Returns an array containing the invalid variable names, <br>
+	 * or null if all the names are valid.
+	 * @param variables
+	 * @return
+	 */
+	public final static String[] getInvalidVariableNames(Variable[] variables)
+	{
+		LinkedList<String> invalidVariables = null ; 
+		if ( null == variables ) return null ;
+		for ( int i = 0 ; i < variables.length ; i++ )
+		{
+			Variable v = variables[i];
+			String sVarName = v.getName();
+			if ( isReservedName(sVarName) )
+			{
+				if ( null == invalidVariables )
+				{
+					invalidVariables = new LinkedList<String>();
+				}
+				invalidVariables.add(sVarName);
+			}
+		}
+		if ( invalidVariables != null ) return (String[]) invalidVariables.toArray(new String[0]);
+		else return null ;
+	}
+}
