6f3b5af028fa665e181fa8314e3a36d3f92114a0
laurent.guer1@gmail.com <laurent.guer1@gmail.com@aaae8943-b729-654d-e607-ed1768d4292a>
2013-10-14 15:21:11 +0000

    Specific error message depending on the exception type (including the new DirectiveException)

--- trunk/TelosysToolsPlugin/src/org/telosys/tools/eclipse/plugin/editors/dbrep/GenerationTask.java
+++ trunk/TelosysToolsPlugin/src/org/telosys/tools/eclipse/plugin/editors/dbrep/GenerationTask.java
@@ -3,6 +3,9 @@ package org.telosys.tools.eclipse.plugin.editors.dbrep;
 import java.lang.reflect.InvocationTargetException;
 import java.util.LinkedList;
 
+import org.apache.velocity.exception.MethodInvocationException;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.telosys.tools.commons.ConsoleLogger;
@@ -11,9 +14,11 @@ import org.telosys.tools.eclipse.plugin.commons.MsgBox;
 import org.telosys.tools.eclipse.plugin.commons.TelosysPluginException;
 import org.telosys.tools.eclipse.plugin.commons.Util;
 import org.telosys.tools.eclipse.plugin.config.ProjectConfig;
+import org.telosys.tools.generator.GeneratorContextException;
 import org.telosys.tools.generator.GeneratorException;
 import org.telosys.tools.generator.config.GeneratorConfigManager;
 import org.telosys.tools.generator.config.IGeneratorConfig;
+import org.telosys.tools.generator.directive.DirectiveException;
 import org.telosys.tools.generator.target.TargetDefinition;
 import org.telosys.tools.repository.model.RepositoryModel;
 
@@ -82,13 +87,89 @@ public class GenerationTask {
 			
 			MsgBox.info("Normal end of generation\n\n" + generationTask.getResult() + " file(s) generated.");
 			
-		} catch (InvocationTargetException e) {
-			MsgBox.error("Error during generation", e.getCause() );
+		} catch (InvocationTargetException invocationTargetException) {
+			//MsgBox.error("Error during generation", e.getCause() );
+			showGenerationError(invocationTargetException); // v 2.0.7
 		} catch (InterruptedException e) {
 			MsgBox.info("Generation interrupted");
 		}
 		
     	return generationTask.getResult();
+		
+	}
+	
+	/**
+	 * Specific message depending on the type of exception
+	 * @param invocationTargetException
+	 * @since 2.0.7
+	 */
+	private void showGenerationError(InvocationTargetException invocationTargetException) {
+		Throwable cause = invocationTargetException.getCause();
+		if ( cause instanceof GeneratorException ) {
+			GeneratorException generatorException = (GeneratorException) cause ;
+			Throwable generatorExceptionCause = generatorException.getCause() ;
+			
+			if ( generatorExceptionCause instanceof DirectiveException ) {
+				//--- DIRECTIVE ERROR
+				DirectiveException directiveException = (DirectiveException) generatorExceptionCause ;
+				String msg = "Directive error ( #" + directiveException.getDirectiveName() + " ) \n\n" 
+					+ directiveException.getMessage() 
+					+ "\n\n" 
+					+ "Template : " + directiveException.getTemplateName() 
+					+ " ( line " + directiveException.getLineNumber() + " )" ;  
+				MsgBox.error( msg );
+			}
+			else if ( generatorExceptionCause instanceof ParseErrorException ) {
+				//--- TEMPLATE PARSING ERROR
+				ParseErrorException parseErrorException = (ParseErrorException) generatorExceptionCause ;
+				String msg = "Template parsing error \n\n" 
+					+ parseErrorException.getMessage() 
+					+ "\n\n" 
+					+ "Template : " + parseErrorException.getTemplateName() 
+					+ " ( line " + parseErrorException.getLineNumber() + " )" 
+					+ "\n\n" 
+					+ "Invalid syntax : \n" + parseErrorException.getInvalidSyntax()  
+					;
+				MsgBox.error( msg );
+			}
+			else if ( generatorExceptionCause instanceof MethodInvocationException ) {
+				//--- METHOD INVOCATION
+				MethodInvocationException methodInvocationException = (MethodInvocationException) generatorExceptionCause ;
+				String msg = "Method invocation error \n\n" 
+					+ methodInvocationException.getMessage() 
+					+ "\n\n" 
+					+ "Template : " + methodInvocationException.getTemplateName() 
+					+ " ( line " + methodInvocationException.getLineNumber() + " )" 
+					+ "\n\n" 
+					+ "Method name : \n" + methodInvocationException.getMethodName()
+					+ "Reference name : \n" + methodInvocationException.getReferenceName()
+					;
+				MsgBox.error( msg );
+			}			
+			else if ( generatorExceptionCause instanceof ResourceNotFoundException ) {
+				//--- RESOURCE NOT FOUND
+				ResourceNotFoundException resourceNotFoundException = (ResourceNotFoundException) generatorExceptionCause ;
+				String msg = "Resource not found \n\n" 
+					+ resourceNotFoundException.getMessage() 
+					;
+				MsgBox.error( msg );
+			}			
+			else if ( generatorExceptionCause instanceof GeneratorContextException ) {
+				//--- CONTEXT ERROR
+				GeneratorContextException generatorContextException = (GeneratorContextException) generatorExceptionCause ;
+				String msg = "Context error \n\n" 
+					+ generatorContextException.getMessage() 
+					;
+				MsgBox.error( msg );
+			}
+			else {
+				MsgBox.error("Error during generation", cause );
+			}
+		}
+		else {
+			
+			MsgBox.error("Error during generation", cause );
+		}
 		
 	}
 }
