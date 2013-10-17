8b2f69ad0ae499e9282b1f919dd2c8c087ed395c
laurent.guer1 <laurent.guer1@gmail.com>
2013-10-17 13:56:51 +0000

    Velocity editor : code assist / directives  . VelocityKeyWord (code cleaning )  . VelocityKeyWords  . ContentAssistMessages  . ContentAssistMessages.properties

--- TelosysToolsPlugin/src/org/telosys/tools/eclipse/plugin/editors/velocity/model/VelocityKeyWords.java
+++ TelosysToolsPlugin/src/org/telosys/tools/eclipse/plugin/editors/velocity/model/VelocityKeyWords.java
@@ -34,23 +34,41 @@ public class VelocityKeyWords {
 		return keyWords;
 	}
 	
+	private static String docHTML(String docId) {
+		return ContentAssistMessages.getString(docId);
+	}
+	
 	private static void initKeyWords() {
 		
 		// TODO Better on XML file for example
+		// Keep alphabetical order
 		keyWords = new ArrayList<VelocityKeyWord>();
-		keyWords.add(new VelocityKeyWord("#break", "#break", ContentAssistMessages.getString(ContentAssistMessages.DIRECTIVE_BREAK_DOC),PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#define", "#define()", ContentAssistMessages.getString(ContentAssistMessages.DIRECTIVE_DEFINE_DOC),PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#else", "#else", null,PluginImages.VELOCITY_DIRECTIVE));
+
+		// Telosys specific directives (added in ver 2.0.7 )
+		keyWords.add(new VelocityKeyWord("#assertFalse", "#assertFalse()", docHTML(ContentAssistMessages.DIRECTIVE_ASSERTFALSE_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#assertTrue",  "#assertTrue()", docHTML(ContentAssistMessages.DIRECTIVE_ASSERTTRUE_DOC),PluginImages.VELOCITY_DIRECTIVE));
+
+		keyWords.add(new VelocityKeyWord("#break",  "#break",    docHTML(ContentAssistMessages.DIRECTIVE_BREAK_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#define", "#define()", docHTML(ContentAssistMessages.DIRECTIVE_DEFINE_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#else",   "#else",     null,PluginImages.VELOCITY_DIRECTIVE));
 		keyWords.add(new VelocityKeyWord("#elseif", "#elseif()", null,PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#end", "#end", null,PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#evaluate", "#evaluate()", ContentAssistMessages.getString(ContentAssistMessages.DIRECTIVE_EVALUATE_DOC),PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#foreach", "#foreach()", ContentAssistMessages.getString(ContentAssistMessages.DIRECTIVE_FOREACH_DOC),PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#if", "#if()", ContentAssistMessages.getString(ContentAssistMessages.DIRECTIVE_IF_DOC),PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#include", "#include()", ContentAssistMessages.getString(ContentAssistMessages.DIRECTIVE_INCLUDE_DOC),PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#macro", "#macro()", ContentAssistMessages.getString(ContentAssistMessages.DIRECTIVE_MACRO_DOC),PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#parse", "#parse()", ContentAssistMessages.getString(ContentAssistMessages.DIRECTIVE_PARSE_DOC),PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#set", "#set()", ContentAssistMessages.getString(ContentAssistMessages.DIRECTIVE_SET_DOC),PluginImages.VELOCITY_DIRECTIVE));
-		keyWords.add(new VelocityKeyWord("#stop", "#stop", ContentAssistMessages.getString(ContentAssistMessages.DIRECTIVE_STOP_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#end",    "#end",      null,PluginImages.VELOCITY_DIRECTIVE));
+		
+		// Telosys specific directives (added in ver 2.0.7 )
+		keyWords.add(new VelocityKeyWord("#error",  "#error()", docHTML(ContentAssistMessages.DIRECTIVE_ERROR_DOC),PluginImages.VELOCITY_DIRECTIVE));
+
+		keyWords.add(new VelocityKeyWord("#evaluate", "#evaluate()", docHTML(ContentAssistMessages.DIRECTIVE_EVALUATE_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#foreach",  "#foreach()", docHTML(ContentAssistMessages.DIRECTIVE_FOREACH_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#if",       "#if()", docHTML(ContentAssistMessages.DIRECTIVE_IF_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#include",  "#include()", docHTML(ContentAssistMessages.DIRECTIVE_INCLUDE_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#macro",    "#macro()", docHTML(ContentAssistMessages.DIRECTIVE_MACRO_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#parse",    "#parse()", docHTML(ContentAssistMessages.DIRECTIVE_PARSE_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#set",      "#set()", docHTML(ContentAssistMessages.DIRECTIVE_SET_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		keyWords.add(new VelocityKeyWord("#stop",     "#stop", docHTML(ContentAssistMessages.DIRECTIVE_STOP_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		
+		// Telosys specific directives (added in ver 2.0.7 )
+		keyWords.add(new VelocityKeyWord("#using",  "#using()", docHTML(ContentAssistMessages.DIRECTIVE_USING_DOC),PluginImages.VELOCITY_DIRECTIVE));
+		
 	}
 
 }
