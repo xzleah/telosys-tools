7cad8fc2cb59a7f18fd9a97363349d10c12f1514
laurent.guer1 <laurent.guer1@gmail.com>
2013-11-26 11:29:53 +0000

    GeneratorContextException : template name and line number

--- telosys-tools-generator/src/main/java/org/telosys/tools/generator/GeneratorContextException.java
+++ telosys-tools-generator/src/main/java/org/telosys/tools/generator/GeneratorContextException.java
@@ -15,6 +15,8 @@
  */
 package org.telosys.tools.generator;
 
+import org.apache.velocity.util.introspection.Info;
+
 /**
  * Exception thrown if an error occurs when using the context variables  <br>
  * in the Velocity Template (invalid method invocation, etc )
@@ -28,6 +30,9 @@ public class GeneratorContextException extends RuntimeException
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private final int    line ;
+	private final String templateName ;
 
 	/**
      * @param message
@@ -35,14 +40,46 @@ public class GeneratorContextException extends RuntimeException
     public GeneratorContextException(String message)
     {
         super(message);
+        line = 0 ;
+        templateName = "" ;
     }
+
+//    /**
+//     * @param message
+//     * @param cause
+//     */
+//    public GeneratorContextException(String message, Throwable cause)
+//    {
+//        super(message, cause);
+//    }
 
     /**
      * @param message
-     * @param cause
+     * @param introspectionInfo
+     * @since 2.0.7
      */
-    public GeneratorContextException(String message, Throwable cause)
+    public GeneratorContextException(String message, Info introspectionInfo )
     {
-        super(message, cause);
+        super(message);
+        this.line = introspectionInfo.getLine() ;
+        this.templateName = introspectionInfo.getTemplateName() ;
+    }
+    
+    /**
+     * Returns the template line number where the error occurred ( or "" if the template is unknown )
+     * @return
+     * @since 2.0.7
+     */
+    public String getTemplateName() {
+    	return this.templateName ; 
+    }
+    
+    /**
+     * Returns the template line number where the error occurred ( or 0 if the line is unknown )
+     * @return
+     * @since 2.0.7
+     */
+    public int getLineNumber() {
+    	return this.line ;
     }
 }
