5b8bfb32cac8c7b5d60b487b9558e3793a30dbc1
laurent.guer1@gmail.com <laurent.guer1@gmail.com@aaae8943-b729-654d-e607-ed1768d4292a>
2014-07-10 15:00:53 +0000

    New class

---
+++ trunk/telosys-tools-commons/src/test/java/org/telosys/tools/tests/commons/TestsFolders.java
@@ -0,0 +1,20 @@
+package org.telosys.tools.tests.commons;
+
+public class TestsFolders {
+
+	private final static String TESTS_ROOT_FOLDER = "D:/tmp/telosys-tools-tests" ;
+	
+	public final static String getTestsRootFolder() {
+		return TESTS_ROOT_FOLDER ;
+	}
+	
+	public final static String getTestsBundlesFolder() {
+		return TESTS_ROOT_FOLDER + "/TelosysTools/templates";
+	}
+	
+	public final static String getTestsDownloadFolder() {
+		return TESTS_ROOT_FOLDER + "/TelosysTools/downloads";
+	}
+	
+	
+}
