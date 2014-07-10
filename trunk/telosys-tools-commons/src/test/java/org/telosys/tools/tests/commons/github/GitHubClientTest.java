7a3c542082f9f7ef1d5c43c49962049aec083e50
laurent.guer1@gmail.com <laurent.guer1@gmail.com@aaae8943-b729-654d-e607-ed1768d4292a>
2014-07-10 15:06:14 +0000

    Repo Download

--- trunk/telosys-tools-commons/src/test/java/org/telosys/tools/tests/commons/github/GitHubClientTest.java
+++ trunk/telosys-tools-commons/src/test/java/org/telosys/tools/tests/commons/github/GitHubClientTest.java
@@ -7,33 +7,43 @@ import junit.framework.TestCase;
 
 import org.telosys.tools.commons.github.GitHubClient;
 import org.telosys.tools.commons.github.GitHubRepository;
+import org.telosys.tools.commons.github.GitHubUtil;
+import org.telosys.tools.tests.commons.TestsFolders;
 import org.telosys.tools.tests.commons.http.HttpTestConfig;
 
 public class GitHubClientTest extends TestCase {
 
+	private final static String GITHUB_USER = "telosys-tools" ;
+	// "telosys-tools-beta" ;
+		
 	public void testGetRepositories() {
 		
 		System.out.println("Getting repositories... ");
 
-		String sGitHubUserName = "telosys-tools" ;
-		//String sGitHubUserName = "telosys-tools-beta" ;
-		
-//		Properties properties = new Properties();
-//		properties.setProperty("http.proxyHost",  "proxyitx.idf.fr.ad.sotranet.net");
-//		properties.setProperty("http.proxyPort",  "8080");
-//		properties.setProperty("https.proxyHost", "proxyitx.idf.fr.ad.sotranet.net");
-//		properties.setProperty("https.proxyPort", "8080");
 		Properties properties = HttpTestConfig.getSpecificProxyProperties();
 		
-		GitHubClient gitHubClient = new GitHubClient(properties);
-		String jsonResult = gitHubClient.getRepositoriesJSON(sGitHubUserName);
+		GitHubClient gitHubClient = new GitHubClient(GitHubUtil.getDefaultGitHubURLPattern(), properties);
+		String jsonResult = gitHubClient.getRepositoriesJSON(GITHUB_USER);
 		System.out.println(jsonResult);
 		
-		List<GitHubRepository> repositories = gitHubClient.getRepositories(sGitHubUserName);
+		List<GitHubRepository> repositories = gitHubClient.getRepositories(GITHUB_USER);
 		System.out.println("Repositories (" + repositories.size() + ") : ");
 		for ( GitHubRepository repo : repositories ) {
 			System.out.println(" .  '" + repo.getName() + "' / " 
 					+ repo.getId() + " / '" + repo.getDescription() + "' / " + repo.getSize() );
 		}
+	}
+	
+	public void testDownloadRepository() {
+		
+		Properties properties = HttpTestConfig.getSpecificProxyProperties();		
+		GitHubClient gitHubClient = new GitHubClient(GitHubUtil.getDefaultGitHubURLPattern(), properties);
+		
+		String repoName = "basic-templates-TT210" ;
+		String destinationFile = TestsFolders.getTestsDownloadFolder() + "/" + repoName + ".zip" ;
+		System.out.println("Download repository " + repoName );
+		System.out.println("                 to " + destinationFile );
+		gitHubClient.downloadRepository(GITHUB_USER, repoName, destinationFile);
+		System.out.println("Done.");		
 	}
 }
