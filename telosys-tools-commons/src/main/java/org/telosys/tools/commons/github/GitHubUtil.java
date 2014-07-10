e142e69f07391b5c44c809baa24bf7bdc31f9209
laurent.guer1 <laurent.guer1@gmail.com>
2014-07-10 07:47:53 +0000

    Refactoring : moved from Eclipse plugin to commons + changes

---
+++ telosys-tools-commons/src/main/java/org/telosys/tools/commons/github/GitHubUtil.java
@@ -0,0 +1,21 @@
+package org.telosys.tools.commons.github;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class GitHubUtil {
+
+	public static void sortByName( List<GitHubRepository> list ) {
+		Collections.sort(list, new Comparator<GitHubRepository>() {
+
+			@Override
+			public int compare(GitHubRepository repo1, GitHubRepository repo2) {
+				String name1 = repo1.getName();
+				String name2 = repo2.getName();
+				return name1.compareTo(name2);
+			}
+			
+		});
+	}
+}
