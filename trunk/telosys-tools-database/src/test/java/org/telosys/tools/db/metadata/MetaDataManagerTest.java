9aa7e8549e0fad5546c70d68dbf89fe9e4e82510
laurent.guer1@gmail.com <laurent.guer1@gmail.com@aaae8943-b729-654d-e607-ed1768d4292a>
2014-08-27 09:22:33 +0000

    Column comment + regexp filter ( LCH )

---
+++ trunk/telosys-tools-database/src/test/java/org/telosys/tools/db/metadata/MetaDataManagerTest.java
@@ -0,0 +1,155 @@
+package org.telosys.tools.db.metadata;
+
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class MetaDataManagerTest {
+	
+	@Test
+	public void testIsExcludedTable_patterns_null() {
+		// Given
+		MetaDataManager m = new MetaDataManager(null);
+		
+		String tableName = "abcde";
+		Pattern patternTableNameExclude = null;
+		Pattern patternTableNameInclude = null;
+		
+		// When
+		boolean isExcluded = m.isExcludedTable(tableName, patternTableNameInclude, patternTableNameExclude);
+		
+		// Then
+		Assert.assertFalse(isExcluded);
+	}
+
+	@Test
+	public void testIsExcludedTable_not_excluded() {
+		// Given
+		MetaDataManager m = new MetaDataManager(null);
+		
+		String tableName = "abcde";
+		Pattern patternTableNameExclude = Pattern.compile("test");
+		Pattern patternTableNameInclude = null;
+		
+		// When
+		boolean isExcluded = m.isExcludedTable(tableName, patternTableNameInclude, patternTableNameExclude);
+		
+		// Then
+		Assert.assertFalse(isExcluded);
+	}
+
+	@Test
+	public void testIsExcludedTable_excluded() {
+		// Given
+		MetaDataManager m = new MetaDataManager(null);
+		
+		String tableName = "abcde";
+		Pattern patternTableNameExclude = Pattern.compile("a.*");
+		Pattern patternTableNameInclude = null;
+		
+		// When
+		boolean isExcluded = m.isExcludedTable(tableName, patternTableNameInclude, patternTableNameExclude);
+		
+		// Then
+		Assert.assertTrue(isExcluded);
+	}
+
+	@Test
+	public void testIsExcludedTable_not_included() {
+		// Given
+		MetaDataManager m = new MetaDataManager(null);
+		
+		String tableName = "abcde";
+		Pattern patternTableNameExclude = null;
+		Pattern patternTableNameInclude = Pattern.compile("test");
+		
+		// When
+		boolean isExcluded = m.isExcludedTable(tableName, patternTableNameInclude, patternTableNameExclude);
+		
+		// Then
+		Assert.assertTrue(isExcluded);
+	}
+
+	@Test
+	public void testIsExcludedTable_included() {
+		// Given
+		MetaDataManager m = new MetaDataManager(null);
+		
+		String tableName = "abcde";
+		Pattern patternTableNameExclude = null;
+		Pattern patternTableNameInclude = Pattern.compile("a.*");
+		
+		// When
+		boolean isExcluded = m.isExcludedTable(tableName, patternTableNameInclude, patternTableNameExclude);
+		
+		// Then
+		Assert.assertFalse(isExcluded);
+	}
+
+	@Test
+	public void testIsExcludedTable_excluded_and_included() {
+		// Given
+		MetaDataManager m = new MetaDataManager(null);
+		
+		String tableName = "abcde";
+		Pattern patternTableNameExclude = Pattern.compile("a.*");
+		Pattern patternTableNameInclude = Pattern.compile("a.*");
+		
+		// When
+		boolean isExcluded = m.isExcludedTable(tableName, patternTableNameInclude, patternTableNameExclude);
+		
+		// Then
+		Assert.assertTrue(isExcluded);
+	}
+
+	@Test
+	public void testIsExcludedTable_excluded_and_not_included() {
+		// Given
+		MetaDataManager m = new MetaDataManager(null);
+		
+		String tableName = "abcde";
+		Pattern patternTableNameExclude = Pattern.compile("a.*");
+		Pattern patternTableNameInclude = Pattern.compile("test");
+		
+		// When
+		boolean isExcluded = m.isExcludedTable(tableName, patternTableNameInclude, patternTableNameExclude);
+		
+		// Then
+		Assert.assertTrue(isExcluded);
+	}
+
+	@Test
+	public void testIsExcludedTable_not_excluded_and_included() {
+		// Given
+		MetaDataManager m = new MetaDataManager(null);
+		
+		String tableName = "abcde";
+		Pattern patternTableNameExclude = Pattern.compile("test");
+		Pattern patternTableNameInclude = Pattern.compile("a.*");
+		
+		// When
+		boolean isExcluded = m.isExcludedTable(tableName, patternTableNameInclude, patternTableNameExclude);
+		
+		// Then
+		Assert.assertFalse(isExcluded);
+	}
+
+	@Test
+	public void testIsExcludedTable_not_excluded_and_not_included() {
+		// Given
+		MetaDataManager m = new MetaDataManager(null);
+		
+		String tableName = "abcde";
+		Pattern patternTableNameExclude = Pattern.compile("test");
+		Pattern patternTableNameInclude = Pattern.compile("test");
+		
+		// When
+		boolean isExcluded = m.isExcludedTable(tableName, patternTableNameInclude, patternTableNameExclude);
+		
+		// Then
+		Assert.assertTrue(isExcluded);
+	}
+	
+}
